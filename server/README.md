345bcf422d176a59495d453ac8824a7774c21cae
Joey-Coder <Joey950326@gmail.com>
2020-09-29 13:40:59 +0800

    update ajax example

---
+++ server/README.md
@@ -0,0 +1,15 @@
+# 文件清单
+
+01.html ajax 入门
+
+02.html 传递 get 请求参数
+
+03.html 传递 post 请求参数
+
+04.html 服务端向客户端传递 json
+
+05.html Ajax 状态码
+
+06.html Ajax 错误处理
+
+07.html Ajax 缓存 
--- server/app.js
+++ server/app.js
@@ -1,15 +1,53 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
+const fs = require("fs");
 
 const app = express();
+
+// parse application/json
+app.use(bodyParser.json());
+// urlencoded类型
+// app.use(bodyParser.urlencoded())
 
 // 设置静态资源
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/index", (req, res) => {
-  res.send("Hello World");
+app.get("/first", (req, res) => {
+  //   res.send("Hello Ajax");
+  res.send({ name: "Joey" });
 });
 
+app.get("/get", (req, res) => {
+  res.send(req.query);
+});
+
+app.post("/post", (req, res) => {
+  //   res.send({ name: "Joey" });
+  res.send(req.body);
+});
+
+app.post("/json", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+app.get("/readystate", (req, res) => {
+  console.log("/readystate");
+  res.send("Hello");
+});
+
+app.get("/error", (req, res) => {
+  console.log("/error");
+  // console.log(aaa);
+  // res.status(400).send("something error");
+});
+
+app.get("/cache", (req, res) => {
+  fs.readFile("./test.txt", (err, data) => {
+    !err && res.send(data);
+  });
+});
 app.listen(3000);
 
 console.log("服务器启动成功...");
