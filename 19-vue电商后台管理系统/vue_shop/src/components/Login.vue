5e746a3e8fa5749b4871afff0326652d0b1cedfd
Joey-Coder <Joey950326@gmail.com>
2020-10-28 17:33:49 +0800

    add Login component

---
+++ 19-vue电商后台管理系统/vue_shop/src/components/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>登陆组件</div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+};
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ 19-vue电商后台管理系统/vue_shop/vue.config.js
@@ -0,0 +1,9 @@
+// vue-cli配置文件
+// 用来配置port host
+module.exports = {
+    devServer: {
+      open: true,
+      port: 8878
+    }
+  };
+  
