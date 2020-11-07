c02a84514edb3faed81a2fbd86c7a87b1643f369
Joey-Coder <Joey950326@gmail.com>
2020-11-07 17:57:12 +0800

    init params.vue

---
+++ 19-vue电商后台管理系统/vue_shop/src/components/goods/Params.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    Params Page
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped></style>
