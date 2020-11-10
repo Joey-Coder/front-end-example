fa0205cb3c326a47c51df510e859c5dd1c55a862
Joey-Coder <Joey950326@gmail.com>
2020-11-10 22:33:49 +0800

    使用gzip优化网络传输

--- 19-vue电商后台管理系统/vue_shop_server/app.js
+++ 19-vue电商后台管理系统/vue_shop_server/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const compression = require("compression");
 
 const app = express();
 
+app.use(compression());
 app.use(express.static("./dist"));
 
 app.listen(8011, () => {
