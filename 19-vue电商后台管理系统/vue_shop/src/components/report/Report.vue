964e2bf5fe527ec1b42e3636b71d330cd864faa3
Joey-Coder <Joey950326@gmail.com>
2020-11-10 17:01:23 +0800

    完成报表功能页面

---
+++ 19-vue电商后台管理系统/vue_shop/src/components/report/Report.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card border stripe>
+      <div id="main" style="width: 80%; height:400px"></div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import echarts from 'echarts'
+import _ from 'lodash'
+export default {
+  data() {
+    return {
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
+  },
+  // 页面元素被渲染完毕
+  async mounted() {
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'))
+    const { data: res } = await this.$http.get('reports/type/1')
+    if (res.meta.status !== 200) {
+      return this.$message.error('获取报表数据失败')
+    }
+    // 使用lodash合并报表样式和报表数据
+    const result = _.merge(res.data, this.options)
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(result)
+  },
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped></style>
--- 19-vue电商后台管理系统/vue_shop/src/router/index.js
+++ 19-vue电商后台管理系统/vue_shop/src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params.vue'
 import List from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 
 Vue.use(VueRouter)
 
@@ -39,7 +40,9 @@ const routes = [
       // 添加商品
       { path: '/goods/add', component: Add },
       // 订单列表
-      { path: '/orders', component: Order }
+      { path: '/orders', component: Order },
+      // 报表
+      { path: '/reports', component: Report }
     ]
   }
 ]
