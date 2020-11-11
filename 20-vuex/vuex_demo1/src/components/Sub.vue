7be8725ee0bf625a856570de46ce8c58eb65d220
Joey-Coder <Joey950326@gmail.com>
2020-11-11 16:35:23 +0800

    vuex核心概念初识

--- 20-vuex/vuex_demo1/src/components/Sub.vue
+++ 20-vuex/vuex_demo1/src/components/Sub.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-    <h3>当前最新的count为：</h3>
-    <button>-1</button>
+    <!-- 通过this.$state使用state -->
+    <h3>当前最新的count为： {{ this.$store.state.count }}</h3>
+    <h3>getter： {{ this.$store.getters.showNum }}</h3>
+    <button @click="btnHandle">-1</button>
+    <button @click="btnHandleN">-3</button>
+    <button @click="AsyncHandle">异步-1</button>
+    <button @click="AsyncHandleN">异步-3</button>
   </div>
 </template>
 
@@ -9,6 +14,20 @@
 export default {
   data() {
     return {}
+  },
+  methods: {
+    btnHandle() {
+      this.$store.commit('sub')
+    },
+    btnHandleN() {
+      this.$store.commit('subN', 3)
+    },
+    AsyncHandle() {
+      this.$store.dispatch('subAsync')
+    },
+    AsyncHandleN() {
+      this.$store.dispatch('subAsyncN', 3)
+    }
   }
 }
 </script>
--- 20-vuex/vuex_demo1/src/store/index.js
+++ 20-vuex/vuex_demo1/src/store/index.js
@@ -4,12 +4,61 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // 定义全局数据
   state: {
+    // 定义全局数据count
+    count: 0
   },
+  // 起到对state数据进行包装的作用
+  // 不会修改state的数据
+  getters: {
+    showNum(state) {
+      return '当前count为' + state.count + '...'
+    }
+  },
+  // 定义修改数据的方法
+  // 只有mutation才有修改state的权利
   mutations: {
+    add(state) {
+      state.count++
+    },
+    addN(state, N) {
+      state.count += N
+    },
+    sub(state) {
+      state.count--
+    },
+    subN(state, N) {
+      state.count -= N
+    }
   },
+  // 用于定义异步操作
   actions: {
+    // 异步-1
+    subAsync(context) {
+      setTimeout(() => {
+        // 触发sub函数
+        context.commit('sub')
+      }, 2000)
+    },
+    subAsyncN(context, N) {
+      setTimeout(() => {
+        context.commit('subN', N)
+      }, 2000)
+    },
+    // 异步+1
+    addAsync(context) {
+      setTimeout(() => {
+        // 触发add函数
+        context.commit('add')
+      }, 2000)
+    },
+    // 异步+3
+    addAsyncN(context, N) {
+      setTimeout(() => {
+        context.commit('addN', N)
+      }, 2000)
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
