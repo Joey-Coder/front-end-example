20a459b781da24aa16478138f515720fe297cd83
Joey-Coder <Joey950326@gmail.com>
2020-11-11 15:22:17 +0800

    init vuex

---
+++ 20-vuex/vuex_demo1/src/components/Add.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h3>当前最新的count为：</h3>
+    <button>+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
