e4c5c2e162902aca006561401489f7644012c879
Joey-Coder <Joey950326@gmail.com>
2020-11-29 20:00:52 +0800

    添加登录路由守护

--- 21-vue头条移动端/toutiao/src/router/index.js
+++ 21-vue头条移动端/toutiao/src/router/index.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import { Dialog } from 'vant'
+import store from '@/store'
 
 Vue.use(VueRouter)
 
 // 配置路由表
 const routes = [
   // 路由懒加载
-  { path: '/login', name: 'login', component: () => import('@/views/login/') },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login/'),
+    meta: { requiresAuth: false }
+  },
   // 含有底边栏的页面
   {
     path: '/',
@@ -15,22 +22,26 @@ const routes = [
       {
         path: '', // 默认子路由，
         name: 'home',
-        component: () => import('@/views/home/')
+        component: () => import('@/views/home/'),
+        meta: { requiresAuth: false }
       },
       {
         path: '/question',
         name: 'question',
-        component: () => import('@/views/question/')
+        component: () => import('@/views/question/'),
+        meta: { requiresAuth: false }
       },
       {
         path: '/video',
         name: 'video',
-        component: () => import('@/views/video/')
+        component: () => import('@/views/video/'),
+        meta: { requiresAuth: false }
       },
       {
         path: '/my',
         name: 'my',
-        component: () => import('@/views/my/')
+        component: () => import('@/views/my/'),
+        meta: { requiresAuth: false }
       }
     ]
   },
@@ -52,12 +63,36 @@ const routes = [
   {
     path: '/user/profile',
     name: 'user-profile',
-    component: () => import('@/views/user-profile')
+    component: () => import('@/views/user-profile'),
+    meta: { requiresAuth: true }
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  // 如果以登录，则直接通过
+  if (store.state.user) {
+    return next()
+  }
+  // 需要登录
+  if (to.meta.requiresAuth) {
+    Dialog.confirm({
+      title: '访问提示',
+      message: '该功能需要登录才能访问，需要登录么？'
+    })
+      .then(() => {
+        next()
+      })
+      .catch(() => {
+        // 取消，中断导航
+        next(false)
+      })
+  } else {
+    next()
+  }
 })
 
 export default router
