e5add7a06553c17a7dee8e591fcb999237d0354c
Joey-Coder <joey950326@gmail.com>
2020-12-04 00:49:22 +0800

    finish 11.js

--- js基础与进阶/10.js
+++ js基础与进阶/10.js
@@ -2,6 +2,7 @@
  * instanceof方法原理：
  * 构造函数的显式原型在实例对象的隐式原型链上就返回true
  * Object的构造函数是Function原型对象的实例对象，而Function的原型对象由Object的原型构造函数产生
+ * 构造函数比较特殊，构造函数是函数原型的实例，而函数原型是原型对象的实例：Fn -> Function -> Object -> null
  */
 
 function Fn() {}
@@ -15,3 +16,37 @@ console.log(Function instanceof Function); // true
 console.log(Function instanceof Object); // true
 
 console.log(Object instanceof Fn); // false
+
+/**
+ * 测试题 01
+ */
+
+function A() {}
+A.prototype.n = 1;
+var b = new A();
+A.prototype = {
+  n: 2,
+  m: 3,
+}; // 修改显式原型的地址，但是实例对象b的__proto__并未被修改
+var c = new A();
+console.log(b.m, b.n); // undefined 1
+console.log(c.m, c.n); // 3 2
+
+/**
+ * 测试题 00
+ */
+
+function F() {}
+Object.prototype.a = function () {
+  console.log("a");
+};
+Function.prototype.b = function () {
+  console.log("b");
+};
+
+var f = new F();
+
+f.a(); // a
+f.b(); // undefined
+F.a(); // a
+F.b(); // b
---
+++ js基础与进阶/11.js
@@ -0,0 +1,18 @@
+/**
+ * 变量提升: 在程序一开始优先执行变量，函数定义，再顺序其他语句
+ */
+
+var a = 0;
+function fn() {
+  console.log(a);
+  var a = 4; // 事实上，先在log之前var a，然后再log后a=4
+}
+fn(); // undefined
+
+console.log(b);
+
+fn2(); // fn2 可以正常调用，因为函数提升
+function fn2() {
+  console.log("fn2");
+}
+var b = 3; // undefined 不报错
